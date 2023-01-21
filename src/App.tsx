d18f01865b122bdb2659a937ef117fde63dda128
Stanislav Moskalenko <maskal2719@gmail.com>
2023-01-22 02:50:05 +0300

    feat: add Header and Technologies component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="App">
+            <Header/>
+            Hello, samurai! Let's go!
+            <Technologies/>
+        </div>
+    );
+}
+const Header = () => {
+    return (
+        <div>
+            <a href="#s">Home</a>
+            <a href="#s">Home</a>
+            <a href="#s">Home</a>
+        </div>
+    );
+}
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>JS</li>
+                <li>CSS</li>
+                <li>HTML</li>
+                <li>React</li>
+            </ul>
+        </div>
+    );
 }
 
 export default App;
