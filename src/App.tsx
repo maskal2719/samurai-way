027b28f7ae63916fdddf2350ae504ae812b7a302
Stanislav Moskalenko <maskal2719@gmail.com>
2023-02-01 15:26:59 +0300

    complete lesson 11

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,48 @@
 import React from 'react';
 import './App.css';
-import Technologies from "./Technologies";
-import Header from "./Header";
 
 const App = () => {
     return (
-        <div className="App">
-            <Header/>
-            Hello, samurai! Let's go!
-            <Technologies/>
+        <div className="container">
+            <header className='header'>
+                <img className='header-logo'
+                     src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Telegram_Messenger.png/640px-Telegram_Messenger.png"
+                     alt=""/>
+            </header>
+            <section className='main-section'>
+                <nav className='nav'>
+                    <div><a href="#">Profile</a></div>
+                    <div><a href="#">Messages</a></div>
+                    <div><a href="#">News</a></div>
+                    <div><a href="#">Music</a></div>
+                    <div><a href="#">Settings</a></div>
+                </nav>
+                <div className='main-content'>
+                    <div>
+                        <img className='fon'
+                             src="https://www.rgo.ru/sites/default/files/node/59064/anna-politova-solnechnyy-ushishir-602387.jpg"
+                             alt=""/>
+                    </div>
+                    <div className='profile-info'>
+                        <img src="https://avatars.githubusercontent.com/u/11731867?v=4" alt=""/>
+                        <div>
+                            <h3>Marco Reus</h3>
+                            <span>27.07.1996</span>
+                        </div>
+                    </div>
+                    <div className='post'>
+                        My posts
+                        <div className='new-post'>
+                            <input type="text"/>
+                        </div>
+                        <div className='post'>
+                            Post1
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+            <footer className='footer'>Footer</footer>
         </div>
     );
 }
