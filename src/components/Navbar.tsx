db0fa15512ce0f201241fed7689e5ed0dbfba312
Stanislav Moskalenko <maskal2719@gmail.com>
2023-02-01 16:59:51 +0300

    complete lesson 14

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import classes from "./Navbar.module.css";
+
 
 const Navbar = () => {
     return (
-        <nav className='nav'>
-            <div><a href="#">Profile</a></div>
-            <div><a href="#">Messages</a></div>
-            <div><a href="#">News</a></div>
-            <div><a href="#">Music</a></div>
-            <div><a href="#">Settings</a></div>
+        <nav className={classes.nav}>
+            <div className={classes.item}><a>Profile</a></div>
+            <div className='item'><a>Messages</a></div>
+            <div className='item'><a>News</a></div>
+            <div className='item'><a>Music</a></div>
+            <div className='item'><a>Settings</a></div>
         </nav>
     );
 };
