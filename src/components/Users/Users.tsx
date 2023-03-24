73dcb525fc932a3bec7306952294edc515aa78e6
Stanislav Moskalenko <maskal2719@gmail.com>
2023-03-24 14:54:57 +0300

    lesson 52- (Side effect)

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -5,17 +5,20 @@ import axios from "axios";
 import userPhoto from '../../assets/images/user.jpg'
 
 const Users: React.FC<UsersPropsType> = ({users, setUsers, unfollow, follow}) => {
-    const urlUsers = 'https://social-network.samuraijs.com/api/1.0/users'
 
-    if (users.length === 0) {
-        axios.get(urlUsers)
-            .then(response => {
-                setUsers(response.data.items)
-            })
+    let getUsers = () => {
+        const urlUsers = 'https://social-network.samuraijs.com/api/1.0/users'
+        if (users.length === 0) {
+            axios.get(urlUsers)
+                .then(response => {
+                    setUsers(response.data.items)
+                })
+        }
     }
 
     let usersElement = users.length > 0 ? users.map(el =>
         <div key={el.id}>
+
             <img style={{width: '80px'}} src={el.photos.small !== null ? el.photos.small : userPhoto} alt="avatar"/>
             {el.name}
             {!el.followed
@@ -23,10 +26,14 @@ const Users: React.FC<UsersPropsType> = ({users, setUsers, unfollow, follow}) =>
                 : <button onClick={() => unfollow(el.id)}>Unfollow</button>
             }
         </div>
-    ) : <div>У тебя Нет друзей, мудак :)</div>
+    ) : <div>
+        <button onClick={getUsers}>Получить</button>
+        У тебя Нет друзей,потому что ты мудак :)))
+    </div>
 
     return (
         <>
+
             {usersElement}
         </>
     );
