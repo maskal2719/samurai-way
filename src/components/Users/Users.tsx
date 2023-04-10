f462d884ae3a4c6c4c01f5fd2ce401483044656c
Stanislav Moskalenko <maskal2719@gmail.com>
2023-04-10 14:48:20 +0300

    add homepage

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import userPhoto from "../../assets/images/user.jpg";
 import axios from "axios";
 import {UsersPropsType} from "./UsersContainer";
+import classes from './Users.module.css'
 
 class Users extends React.Component<UsersPropsType> {
     componentDidMount() {
@@ -13,9 +14,9 @@ class Users extends React.Component<UsersPropsType> {
 
     render() {
         return (
-            <>
+            <div className={classes.users}>
                 {this.props.users.length > 0 ? this.props.users.map(el =>
-                    <div key={el.id}>
+                    <div key={el.id} className={classes.user}>
 
                         <img style={{width: '80px'}} src={el.photos.small !== null ? el.photos.small : userPhoto}
                              alt="user_avatar"/>
@@ -26,7 +27,7 @@ class Users extends React.Component<UsersPropsType> {
                         }
                     </div>
                 ) : <div>У тебя Нет друзей,потому что ты мудак :)))</div>}
-            </>
+            </div>
         );
     }
 }
