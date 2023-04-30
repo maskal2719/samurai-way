5296617e33afd58ff4f594eb4b2662c16035959f
Stanislav Moskalenko <maskal2719@gmail.com>
2023-04-30 16:52:45 +0300

    fix user photo

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import classes from "./Users.module.css";
-import userPhoto from "../../assets/images/user.jpg";
+import userPhoto from "../../assets/images/user.jpeg";
 import {UsersDataType} from "../../redux/users-reducer";
 import {NavLink} from "react-router-dom";
 
