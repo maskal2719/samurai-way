c753aa0a8bddff727122ff306450605b79ee3640
Stanislav Moskalenko <maskal2719@gmail.com>
2023-05-18 09:00:24 +0300

    lesson 83 (reselect part 3)

--- src/components/common/Preloader/Preloader.tsx
+++ src/components/common/Preloader/Preloader.tsx
@@ -3,7 +3,7 @@ import preloader from "../../../assets/images/loading-animation.gif";
 
 const Preloader = () => {
     return (
-        <img style={{width: '200px'}} src={preloader} alt={'prealoader'}/>
+        <img style={{width: '200px', display: "flex", margin: '0 auto', marginTop: '200px'}} src={preloader} alt={'prealoader'}/>
     );
 };
 
